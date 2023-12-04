4c32977f07d3c622e795bd84b717ba6f376d78f0
Pavelavl <sfdrus2004@gmail.com>
2023-12-04 20:25:22 +0300

    Добавьте файлы проекта.

---
+++ Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Generics;
+
+namespace Prog
+{
+    class Prog
+    {
+        void Main()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(3);
+            list.Add(4);
+            list.Add(2);
+
+            Generics.Generics.Sort<int>(list);
+        }
+    }
+}
---
+++ Generics/Generics.cs
@@ -0,0 +1,26 @@
+﻿namespace Generics
+{
+    public static class Generics
+    {
+        public interface IComparable<in T>
+        {
+            int CompareTo(T other);
+        }
+        public static void Sort<T>(this IList<T> list) where T : IComparable<T>
+        {
+            int n = list.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (list[j].CompareTo(list[j + 1]) > 0)
+                    {
+                        T temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
