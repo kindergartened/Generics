e6df9c0e1283d1424a10039f403483f18492e628
TheRomaks <niks_2004@bk.ru>
2023-12-06 20:19:44 +0300

    nasral lib

--- Generics/Generics.cs
+++ Generics/Generics.cs
@@ -32,7 +32,7 @@
         {
             int left = 0;
             int right = list.Count - 1;
-            SortArray(list, left, right); 
+            SortArray(list, left, right);
         }
         /// <summary>
         /// вспомогательный метод для QuickSort
@@ -64,5 +64,95 @@
             if (i < right)
                 SortArray(list, i, right);
         }
+        /// <summary>
+        /// Сортировка вставками
+        /// </summary>
+        /// <typeparam name="T">тип элементов списка</typeparam>
+        /// <param name="list">список элементов</param>
+        public static void Vstavka<T>(this List<T> list) where T : IComparable<T>
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                T k = list[i];
+                int j = i - 1;
+                while (j >= 0 && list[j].CompareTo(k) < 0)
+                {
+                    list[j + 1] = list[j];
+                    j--;
+                }
+                list[j + 1] = k;
+            }
+        }
+        /// <summary>
+        /// Сортировка слиянием
+        /// </summary>
+        /// <typeparam name="T">тип элементов списка</typeparam>
+        /// <param name="list">список элементов</param>
+        public static void Sliyaniesort<T>(this List<T> list) where T : IComparable<T>
+        {
+            int left = 0;
+            int right = list.Count - 1;
+            Mergesort(list, left, right);
+        }
+        /// <summary>
+        /// вспомогательный метод для сортировки слиянием
+        /// </summary>
+        /// <typeparam name="T">тип элементов списка</typeparam>
+        /// <param name="list">список элементов</param>
+        /// <param name="left">левая граница</param>
+        /// <param name="right">правая граница</param>
+        static void Mergesort<T>(this List<T> list, int left, int right) where T : IComparable<T>
+        {
+            if (right - left > 1)
+            {
+                int a1 = left;
+                int b1 = (left + right) / 2;
+                int a2 = (left + right) / 2 + 1;
+                int b2 = right;
+                Mergesort(list, a1, b1);
+                Mergesort(list, a2, b2);
+                T[] t = new T[right - left + 1];
+                for (int k = left; k <= right; k++)
+                    t[k - left] = list[k];
+                int i = a1;
+                int j = a2;
+                for (int k = left; k <= right; k++)
+                {
+                    if (i <= b1 && j <= b2)
+                    {
+                        if (t[i - left].CompareTo(t[j - left]) < 0)
+                        {
+                            list[k] = t[i - left];
+                            i++;
+                        }
+                        else
+                        {
+                            list[k] = t[j - left];
+                            j++;
+                        }
+                    }
+                    else
+                    {
+                        if (i > b1)
+                        {
+                            list[k] = t[j - left];
+                            j++;
+                        }
+                        else
+                        {
+                            list[k] = t[i - left];
+                            i++;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (right - left == 1)
+                    if (list[left].CompareTo(list[right]) > 0)
+                        (list[left], list[right]) = (list[right], list[left]);
+            }
+        }
+
     }
 }
