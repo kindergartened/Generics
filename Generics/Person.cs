1f3e29c21c6e46236526aa5d18b53184b5f61f66
Pavelavl <sfdrus2004@gmail.com>
2023-12-06 23:59:23 +0300

    added tests

--- Generics/Person.cs
+++ Generics/Person.cs
@@ -11,7 +11,7 @@ namespace Generics
         public string Name { get; set; }
         public int Age { get; set; }
 
-        protected Person(string name, int age)
+        public Person(string name, int age)
         {
             Name = name;
             Age = age;
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,12 +1,80 @@
+using System.Collections.Generic;
+
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        Random rnd = new Random();
         [TestMethod]
-        public void TestMethod1()
+        public void TestStrings()
         {
-
+            List<string> list = new();
+            string ñhars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(new string(Enumerable.Repeat(ñhars, rnd.Next(10, 50)).Select(s => s[rnd.Next(s.Length)]).ToArray()));
+            }
+            List<string> list2 = new(list);
+            list2.Sort();
+            list = UniversalSortings.QuickSort(list);
+            CollectionAssert.AreEqual(list, list2);
+        }
+        [TestMethod]
+        public void TestNums()
+        {
+            List<int> list = new();
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(rnd.Next(-10000, 10000));
+            }
+            List<int> list2 = new(list);
+            list2.Sort();
+            list = UniversalSortings.QuickSort(list);
+            CollectionAssert.AreEqual(list, list2);
+        }
+        [TestMethod]
+        public void TestBools()
+        {
+            List<bool> list = new();
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(rnd.Next(0, 2) == 1 ? true : false);
+            }
+            List<bool> list2 = new(list);
+            list2.Sort();
+            list = UniversalSortings.QuickSort(list);
+            CollectionAssert.AreEqual(list, list2);
+        }
+        [TestMethod]
+        public void TestTeachers()
+        {
+            List<Teacher> list = new();
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(Teacher.Generate());
+            }
+            List<Teacher> list2 = new(list);
+            list2 = list2.OrderBy(p => p.Subject).ToList();
+            list = UniversalSortings.QuickSort(list);
+            List<string> subjs = list.Select(p => p.Subject).ToList();
+            List<string> subjs2 = list2.Select(p => p.Subject).ToList();
+            CollectionAssert.AreEqual(subjs, subjs2);
+        }
+        [TestMethod]
+        public void TestStudents()
+        {
+            List<Student> list = new();
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(Student.Generate());
+            }
+            List<Student> list2 = new(list);
+            list2 = list2.OrderBy(p => p.Grade).ToList();
+            list = UniversalSortings.QuickSort(list);
+            List<string> subjs = list.Select(p => p.Grade).ToList();
+            List<string> subjs2 = list2.Select(p => p.Grade).ToList();
+            CollectionAssert.AreEqual(subjs, subjs2);
         }
     }
 }
